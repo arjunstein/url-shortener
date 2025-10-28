cb3358ac6598282b733d2237ec5b02356f3ad93a
Arjunstein <arjun.gnw09@gmail.com>
2025-10-28 13:20:05 +0700

    ♻️ refactor: Removed tracing info and error logs, added new tracing logs for redirect and not found cases

--- src/presentation/handlers.rs
+++ src/presentation/handlers.rs
@@ -48,8 +48,6 @@ pub async fn redirect_handler(req: &mut Request, res: &mut Response) {
         return;
     }
 
-    tracing::info!("Redirect called with code: {}", code);
-
     let pool = db_pool().clone();
     let repo = PostgresUrlRepository::new(pool);
     let svc = UrlServiceImpl::new(Arc::new(repo));
@@ -57,17 +55,19 @@ pub async fn redirect_handler(req: &mut Request, res: &mut Response) {
     match svc.get_target_url(&code).await {
         Ok(Some(target)) => match HeaderValue::from_str(&target) {
             Ok(val) => {
+                tracing::info!("Redirecting to: {}, using code: {}", target, code);
                 res.status_code(StatusCode::TEMPORARY_REDIRECT);
                 res.headers_mut()
                     .insert(HeaderName::from_static("location"), val);
             }
             Err(_) => {
-                tracing::error!("Invalid redirect location in DB: {}", target);
+                tracing::warn!("Invalid redirect location in DB: {}", target);
                 res.status_code(StatusCode::INTERNAL_SERVER_ERROR);
                 res.render("Invalid stored target URL");
             }
         },
         Ok(None) => {
+            tracing::error!("Not found resource: {}", code);
             res.status_code(StatusCode::NOT_FOUND);
             res.render("Not Found");
         }
