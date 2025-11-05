7da75e37307e6b43dd1fd341cc3a4686aca0dac2
Arjunstein <arjun.gnw09@gmail.com>
2025-11-05 14:34:56 +0700

    🚸 Refactor: pretty docs using salvo_oapi

--- src/application/dtos.rs
+++ src/application/dtos.rs
@@ -2,16 +2,21 @@ use crate::domain::utils::utilities::{
     deserialize_option_datetime, serialize_datetime, serialize_option_datetime,
 };
 use chrono::{DateTime, Utc};
+use salvo::oapi::ToSchema;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, ToSchema)]
+#[salvo(schema(example = json!({
+    "expires_at": "2025-11-05 14:20:30",
+    "target_url": "github.com"
+})))]
 pub struct CreateShortUrlRequest {
     pub target_url: String,
     #[serde(deserialize_with = "deserialize_option_datetime")]
     pub expires_at: Option<DateTime<Utc>>,
 }
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, ToSchema)]
 pub struct CreateUrlResponse {
     pub id: Uuid,
     pub short_code: String,
--- src/presentation/routes.rs
+++ src/presentation/routes.rs
@@ -1,10 +1,11 @@
 use crate::presentation::handlers::{
     create_short_handler, delete_url_handler, get_all_handler, redirect_handler,
 };
+use salvo::oapi::OpenApi;
 use salvo::prelude::*;
 
 pub fn router() -> Router {
-    Router::new()
+    let api_router = Router::new()
         .path("/api/v1")
         .push(
             Router::new()
@@ -13,5 +14,12 @@ pub fn router() -> Router {
                 .get(get_all_handler)
                 .push(Router::new().path("/{code}").delete(delete_url_handler)),
         )
-        .push(Router::new().path("/{code}").get(redirect_handler))
+        .push(Router::new().path("/{code}").get(redirect_handler));
+
+    let doc = OpenApi::default().merge_router(&api_router);
+
+    Router::new()
+        .push(api_router)
+        .push(doc.into_router("/api-doc/openapi.json"))
+        .push(SwaggerUi::new("/api-doc/openapi.json").into_router("/documentation"))
 }
